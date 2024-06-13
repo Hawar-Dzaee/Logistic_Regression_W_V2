068f066433adbae44c06246eb157e7550db255f9
Hawar-Dzaee <hawardizayee@gmail.com>
2024-06-13 19:37:44 -0400

    clean class

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 class LogisticRegression:
 
 
-    def __init__(self ,lower_0, upper_0, sample_size_0, noise_0,lower_1, upper_1, sample_size_1, noise_1):
+    def __init__(self ,lower_0, upper_0, sample_size_0, noise_0,lower_1, upper_1, sample_size_1, noise_1,w):
 
         # Parameters
         self.lower_0 = lower_0
@@ -21,8 +21,9 @@ class LogisticRegression:
         self.upper_1 = upper_1
         self.sample_size_1 = sample_size_1
         self.noise_1 = noise_1
+        self.w = w
 
-        # made by Paramters
+        # made by attributes
         self.x0 = torch.linspace(lower_0, upper_0, sample_size_0) + torch.tensor([noise_0])
         self.x1 = torch.linspace(lower_1, upper_1, sample_size_1) - torch.tensor([noise_1])
         self.X = torch.cat((self.x0, self.x1), dim=0)
@@ -35,61 +36,22 @@ class LogisticRegression:
 
 
 
-
-
-    def model(self,w):
-        self.w = w 
-        L = []
-
-        for weight in self.possible_weights:
-            z = weight * self.X
-            loss = self.loss_fn(z,self.y)
-            L.append(loss)
-
-        L = torch.as_tensor(L)
-        secret_weight = self.possible_weights[torch.argmin(L)]
-
-        return secret_weight,L
     
-    # loss per each class 
-    def loss_per_class(self):
-        loss_class_0 = torch.mean(-torch.log(1-torch.sigmoid(self.w * self.x0)))
-        loss_class_1 =  torch.mean(-torch.log(torch.sigmoid(self.w * self.x1)))
-        loss_class_0_and_1 = loss_class_0 + loss_class_1
-
-        return loss_class_0,loss_class_1,loss_class_0_and_1/2
-    
-    # confusion matrix 
-    def make_predictions(self,threeshold=0.5):
-        with torch.no_grad():
-            prob = torch.sigmoid(self.w * self.X)
-            pred = (prob>threeshold).float()
-            cm = confusion_matrix(self.y,pred,labels=[1,0])
-            disp = ConfusionMatrixDisplay(cm,display_labels=['orange','purple'])
-
-            fig, ax = plt.subplots()
-            disp.plot(ax=ax)
-
-        return fig
-
-
-
-    
-
+    # Data points, sigmoid curve
     def generate_plot(self):
         scatter_class_0 = go.Scatter(
             x=self.x0,
             y=torch.zeros(len(self.x0)),
             mode='markers',
             marker=dict(color='purple'),
-            name='class 0'
+            name='class purple'
         )
         scatter_class_1 = go.Scatter(
             x=self.x1,
             y=torch.ones(len(self.x1)),
             mode='markers',
             marker=dict(color='orange'),
-            name='class 1'
+            name='class orange'
         )
 
         non_linear_line = go.Scatter(
@@ -122,8 +84,44 @@ class LogisticRegression:
         figure = go.Figure(data=[scatter_class_0, scatter_class_1, non_linear_line], layout=layout)
         return figure
     
+#-----------------------------------
+# Math for loss_landscape, loss for classes & confusion matrix 
 
-    def loss_landscape(self,secret_weight,L):
+    def Loss(self):
+        L = []
+
+        for weight in self.possible_weights:
+            z = weight * self.X
+            loss = self.loss_fn(z,self.y)
+            L.append(loss)
+
+        L = torch.as_tensor(L)
+        secret_weight = self.possible_weights[torch.argmin(L)]
+
+        return L
+    
+    # l_class_0 , l_class_1 , l_class_total 
+    def loss_per_class(self):
+        loss_class_0 = torch.mean(-torch.log(1-torch.sigmoid(self.w * self.x0)))
+        loss_class_1 =  torch.mean(-torch.log(torch.sigmoid(self.w * self.x1)))
+        loss_class_0_and_1 = (loss_class_0 + loss_class_1)/2
+
+        return loss_class_0,loss_class_1,loss_class_0_and_1
+    
+    # confusion matrix 
+    def make_predictions(self,threeshold=0.5):
+        with torch.no_grad():
+            prob = torch.sigmoid(self.w * self.X)
+            pred = (prob>threeshold).float()
+            cm = confusion_matrix(self.y,pred,labels=[1,0])
+            disp = ConfusionMatrixDisplay(cm,display_labels=['orange','purple'])
+            
+            disp.plot()
+
+        return plt.gcf()
+#-------------------------------------
+
+    def loss_landscape(self,L):
         
         # landscape
             loss_landscape = go.Scatter(
@@ -136,7 +134,7 @@ class LogisticRegression:
 
             # global
             Global_minima = go.Scatter(
-                x = (secret_weight,),
+                x = (self.possible_weights[torch.argmin(L)],),      # secret weight [The weight that yeilds minimum loss ; Diamond]
                 y = (torch.min(L),),
                 mode = 'markers',
                 marker = dict(color='yellow',size=10,symbol='diamond'),
@@ -177,7 +175,7 @@ class LogisticRegression:
 
 
 
-#---------------------------------------
+#------------------------------------------------------
 # streamlit 
   
 
@@ -190,13 +188,13 @@ with st.sidebar:
 
     st.subheader("Data Generation")
     sample_size_0_val = st.slider("sample size Class 0:", min_value= 2, max_value=12, step=1, value= 3)
-    sample_size_1_val = st.slider("sample size Class 1:", min_value= 2, max_value=12, step=1, value= 3)
+    sample_size_1_val = st.slider("sample size Class 1:", min_value= 2, max_value=12, step=1, value= 3) 
 
     Noise = st.slider('Noise',min_value = 0.0, max_value = 1.0, step = 0.1, value = 0.2)
 
 
     st.subheader("Adjust the parameters to minimize the loss")
-    w_val = st.slider("weight (w):", min_value=-4.0, max_value=18.0, step=0.1, value= -3.5)
+    w_val = st.slider("weight (w):", min_value=-4.0, max_value=18.0, step=0.1, value= -3.5) # first and all the widgets above 
 
 
 container = st.container()
@@ -208,21 +206,22 @@ with container:
     col1, col2 = st.columns([3,3])
 
     with col1:
+
         data = LogisticRegression(lower_0 = -2,upper_0 = 0, sample_size_0 = sample_size_0_val,noise_0 = Noise,
-                                  lower_1 = 0, upper_1 = 2, sample_size_1 = sample_size_1_val, noise_1 = Noise)
-        data.model(w_val)
-        figure_1 = data.generate_plot()
+                                  lower_1 = 0, upper_1 = 2, sample_size_1 = sample_size_1_val, noise_1 = Noise,w = w_val) #second
+        data.Loss() # third
+        figure_1 = data.generate_plot() # fourth
         st.plotly_chart(figure_1, use_container_width=True)
 
         st.latex(r'''\hat{{y}} = \frac{1}{1 + e^{-(\color{green}w\color{black}X)}}''')
         st.latex(fr'''\hat{{y}} = \frac{{1}}{{1 + e^{{-(\color{{green}}{{{w_val}}}\color{{black}}X)}}}}''')  
     
 
-    secret_weight, L = data.model(w_val)
-    loss_class_0,loss_class_1,loss_class_0_and_1 = data.loss_per_class()
+    L = data.Loss() # fifth
+    loss_class_0,loss_class_1,loss_class_0_and_1 = data.loss_per_class() # sixth
 
     with col2:
-       figure_2 = data.loss_landscape(secret_weight,L)
+       figure_2 = data.loss_landscape(L) # seventh
        st.plotly_chart(figure_2,use_container_width=True)
        st.latex(r"""L = -\frac{1}{N} \sum_{i=1}^{N} \left[ y_i \log(\hat{y}_i) + (1 - y_i) \log(1 - \hat{y}_i) \right]""")
        st.latex(rf"""L_{{\text{{class 0}}}} = \textcolor{{purple}}{{{loss_class_0:.4f}}}  \qquad L_{{\text{{class 1}}}} = \textcolor{{orange}}{{{loss_class_1:.4f}}}""")
@@ -230,6 +229,6 @@ with container:
        st.write('---------------')
 
     
-       fig = data.make_predictions()
+       fig = data.make_predictions() #eighth
        st.pyplot(fig)
 
